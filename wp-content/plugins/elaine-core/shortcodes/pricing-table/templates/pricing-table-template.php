61c318f6e7558d9fb63907728f312ff05c4c4d0a
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2020-05-24 17:27:47 +0000

    first commit

---
+++ wp-content/plugins/elaine-core/shortcodes/pricing-table/templates/pricing-table-template.php
@@ -0,0 +1,41 @@
+<div class="edgtf-price-table edgtf-item-space <?php echo esc_attr($holder_classes); ?>">
+	<div class="edgtf-pt-inner" <?php echo elaine_edge_get_inline_style($holder_styles); ?>>
+		<ul>
+		    <li style="padding: 30px; margin-bottom: -50px;">
+		        <?php if (esc_html($title) == 'LALABAG') { ?>
+						<img src="http://test-mauro.site/wp-content/uploads/2020/04/membresia-lalabag-basico.png" alt="" />
+				<?php } else if (esc_html($title) == 'LALABAG PRO') { ?>
+						<img src="http://test-mauro.site/wp-content/uploads/2020/04/membrasia-lalabag-pro.png" alt="" />
+				<?php } else { ?>
+						<img src="http://test-mauro.site/wp-content/uploads/2020/04/Copia-de-LALAENERO22ddddhDd-1-600x600.png" alt="" />
+				<?php } ?>
+			</li>
+			<li class="edgtf-pt-title-holder">
+				<span class="edgtf-pt-title" <?php echo elaine_edge_get_inline_style($title_styles); ?>><?php echo esc_html($title); ?></span>
+			</li>
+			<li class="edgtf-pt-prices">
+				<span class="edgtf-pt-value" <?php echo elaine_edge_get_inline_style($currency_styles); ?>><?php echo esc_html($currency); ?></span>
+				<span class="edgtf-pt-price" <?php echo elaine_edge_get_inline_style($price_styles); ?>><?php echo esc_html($price); ?></span>
+			</li>
+			<li class="edgtf-pt-content">
+				<?php echo do_shortcode($content); ?>
+			</li>
+			<?php 
+			if(!empty($button_text)) { ?>
+				<li class="edgtf-pt-button">
+					<?php echo elaine_edge_get_button_html(array(
+						'link' => $link,
+						'text' => $button_text,
+						'type' => $button_type,
+                        'size' => 'medium',
+						'icon_pack' => "font_elegant",
+						'fe_icon' => "arrow_carrot-right"
+						
+					)); ?>
+					
+					
+				</li>				
+			<?php } ?>
+		</ul>
+	</div>
+</div>
